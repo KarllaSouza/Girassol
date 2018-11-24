7da33aaae0e7b17c2c990e70920faa8a4e4276d0
EikeSan <eikesantz@hotmail.com>
2018-11-23 23:13:28 -0300

    feat(content): criacao do conteudo com, um controler gerenciando as alteracoes

---
+++ www/js/Controller.js
@@ -0,0 +1,64 @@
+var Controller = function() {
+    var controller = {
+        self: null,
+        initialize: function() {
+            self = this;
+            this.bindEvents();
+            self.renderMainView(); 
+        },
+
+        bindEvents: function() {
+            $('.tab-button').on('click', this.onTabClick);
+        },
+
+        onTabClick: function(e) {
+            e.preventDefault();
+            if ($(this).hasClass('active')) {
+                return;
+            }
+            
+            var tab = $(this).data('tab');
+            if (tab === '#add-tab') {
+                self.renderPostView();
+            } else {
+                self.renderSearchView();
+            }
+        },
+
+        renderPostView: function() {
+            $('.tab-button').removeClass('active');
+            $('#post-tab-button').addClass('active');
+
+            var $tab = $('#tab-content');
+            $tab.empty();
+            $("#tab-content").load("./views/post-project-view.html", function(data) {
+                $('#tab-content').find('#post-project-form').on('submit', self.postProject);
+            }); 
+        },
+
+        renderMainView: function() {
+            var $tab = $('#tab-content');
+            $tab.empty();
+            $("#tab-content").load("./views/main-view.html", function(data) {
+                // $('#tab-content').find('#post-project-form').on('submit', self.postProject);
+            }); 
+        },
+       
+       
+        renderSearchView: function() {
+            $('.tab-button').removeClass('active');
+            $('#search-tab-button').addClass('active');
+
+            var $tab = $('#tab-content');
+            $tab.empty();
+
+            var $projectTemplate = null;
+            $("#tab-content").load("./views/search-project-view.html", function(data) {
+                $projectTemplate = $('.project').remove();
+                // Load projects here
+            }); 
+        }
+    }
+    controller.initialize();
+    return controller;
+}
--- www/js/index.js
+++ www/js/index.js
@@ -1,39 +1,32 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var controller = null;
 var app = {
     // Application Constructor
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android)/)) {
+            document.addEventListener("deviceready", this.onDeviceReady, false);
+        } else {
+            this.onDeviceReady();
+        }
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+        controller = new Controller();
+        app.overrideBrowserAlert();
     },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        console.log('Received Event: ' + id);
-    }
+    overrideBrowserAlert: function() {
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Girassol", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    },
+
 };
 
 app.initialize();
