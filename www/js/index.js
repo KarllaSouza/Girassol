aab033ddd425f6366c631c7ad6086d19074eebb4
EikeSan <eikesantz@hotmail.com>
2019-12-10 23:10:34 -0300

    feat(Main-View): Informações do insituto delete
    Adicionado imagem com link para o insituto delete.

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
 var controller = null;
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android)/)) {
             document.addEventListener("deviceready", this.onDeviceReady, false);
         } else {
@@ -9,20 +9,21 @@ var app = {
         }
     },
 
-    onDeviceReady: function() {
-        document.addEventListener("backbutton", function(e){
-            if(document.getElementById('homepage')){
+    onDeviceReady: function () {
+        document.addEventListener("backbutton", function (e) {
+            if (document.getElementById('homepage')) {
                 e.preventDefault();
                 navigator.app.exitApp();
-            } else if ( document.getElementsByClassName('conteudo-texto')){
+            } else if (document.getElementsByClassName('conteudo-texto')) {
                 controller.renderMainView();
             }
-         }, false)
+        }, false)
+        screen.orientation.lock('portrait');
         controller = new Controller();
         app.overrideBrowserAlert();
     },
 
-    overrideBrowserAlert: function() {
+    overrideBrowserAlert: function () {
         if (navigator.notification) { // Override default HTML alert with native dialog
             window.alert = function (message) {
                 navigator.notification.alert(
